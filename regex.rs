1aab64460cc140c9f6a19969643fc8b1279ba0e7
Stef B <regendo@users.noreply.github.com>
2021-12-02 21:13:51 +0100

    Add OnceCell-Regex Macro Combo

---
+++ regex.rs
@@ -0,0 +1,21 @@
+/* Cargo.toml
+"once_cell" = "1.8"
+"regex" = "1.5"
+*/
+
+macro_rules! regex {
+	($re:literal $(,)?) => {{
+		::once_cell::sync::Lazy::<::regex::Regex>::new(|| {
+			regex::RegexBuilder::new($re)
+				.case_insensitive(true)
+				.ignore_whitespace(true)
+				.build()
+				.expect(&format!("Unable to build regex from `{}`.", $re))
+		})
+	}};
+}
+
+pub(crate) use regex;
+
+// In other file
+static MY_REGEX: Lazy<Regex> = regex!(r"^(...)$");
